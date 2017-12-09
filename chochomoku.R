f87a587bbf28a319725b2376b08b7b082307a920
tadakazu1972 <tadakazu1972@gmail.com>
2017-12-09 11:03:19 +0900

    町丁目

---
+++ chochomoku.R
@@ -0,0 +1,8 @@
+#区の町丁目可視化
+#総務省e-Statから世界測地系のshpファイル入手
+
+par(family="HiraKakuProN-W3")
+col_km <- shape$JINKO %>% classIntervals(.,n=8,style=“means”) %>% findColours(.,pal=brewer.pal(8,”Reds”))
+plot(shape[4], col=col_km)
+text(st_coordinates(shape %>% st_centroid)[,1], st_coordinates(shape %>% st_centroid)[,2], labels=shape$MOJI, cex=0.5)
+text(st_coordinates(shape %>% st_centroid)[,1], st_coordinates(shape %>% st_centroid)[,2]-0.001, labels=shape$JINKO, cex=0.5)
