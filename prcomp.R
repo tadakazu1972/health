b14b33c2255558b2e0cad85b45f132dcc34bf2de
tadakazu1972 <tadakazu1972@gmail.com>
2017-12-12 23:03:47 +0900

    prcomp

--- prcomp.R
+++ prcomp.R
@@ -1,4 +1,7 @@
 #主成分分析
+#https://oku.edu.mie-u.ac.jp/~okumura/stat/pca.html
+#http://www.statsbeginner.net/entry/2014/07/27/121214
+
 
 #ファイル読み込み
 data <- read.csv("csv/deathH25.csv")
@@ -7,7 +10,17 @@ data <- read.csv("csv/deathH25.csv")
 row.names(data) = data[,1]
 data = data[,2:17]
 
-#描画２種類
+#描画 ２種類
 par(family="HiraKakuProN-W3")
 biplot(prcomp(data))
 biplot(prcomp(data, scale=TRUE))
+
+#主成分にかかる固有ベクトルがわかる
+result <- prcomp(data, scale=TRUE)
+print(result)
+
+#寄与率と累積寄与率
+summary(result)
+
+#主成分得点を見る
+result$x
